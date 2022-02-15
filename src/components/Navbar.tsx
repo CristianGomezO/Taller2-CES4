8ce53afa1600b6b1da8475e578c13e6aa730dba8
Edward Andres Moron Quinntana <edwaramor9@gmail.com>
2022-02-15 11:42:22 -0500

    se terminan los estilos basicos

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import { Col, Row } from "antd";
+import React from "react";
+
+const styles = {
+  label: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  labelValue: {
+    color: 'white',
+    fontSize: 18,
+  },
+}
 
 interface NavbarProps {
-    
+  playerName: string;
+  earnings: number;
+  dificulty: string;
 }
- 
-const Navbar: React.FC<NavbarProps> = () => {
-    return ( 
-        <div>navbar</div>
-    );
-}
- 
+
+const Navbar: React.FC<NavbarProps> = ({ dificulty, earnings, playerName }) => {
+  return (
+    <Row justify="space-between">
+      <Col>
+        <label style={styles.label}>Player: </label>
+        <label style={styles.labelValue}>{playerName}</label>
+      </Col>
+      <Col>
+        <label style={styles.label}>Level: </label>
+        <label style={styles.labelValue}>{dificulty}</label>
+      </Col>
+      <Col>
+        <label style={styles.label}>Earnings: </label>
+        <label style={styles.labelValue}>{`$ ${earnings}`}</label>
+      </Col>
+    </Row>
+  );
+};
+
 export default Navbar;
--- src/screens/TriviaScreen.tsx
+++ src/screens/TriviaScreen.tsx
@@ -1,14 +1,34 @@
 import { Layout, List } from "antd";
 import React from "react";
 import Navbar from "../components/Navbar";
+import background from "./../assets/images/millionarieImage.jpg";
 
 const styles = {
   list: {
-    height: '100%',
-    display: 'flex',
+    height: "100%",
+    display: "flex",
   },
   sider: {
     backgroundColor: "#030231",
+  },
+  listLabelStyle: {
+    fontSize: 18,
+    color: "#fff",
+    margin: 0,
+  },
+  listItemStyle: {
+    marginLeft: 25,
+    marginTop: 8,
+    borderRadius: 5,
+    borderBottomColor: "#030231",
+    display: "flex",
+    height: 30,
+    width: "80%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerStyle: {
+    backgroundColor: "#512057",
   },
 };
 
@@ -17,53 +37,49 @@ const { Header, Sider, Content } = Layout;
 interface TriviaScreenProps {}
 
 const data = [
-  1000000,
-  500000,
-  250000,
-  125000,
-  64000,
-  32000,
-  16000,
-  8000,
-  4000,
-  2000,
-  1000,
-  500,
-  400,
-  300,
-  200,
-  100,
+  1000000, 900000, 800000, 700000, 600000, 500000, 400000, 300000, 200000,
+  100000,
 ];
 
 const TriviaScreen: React.FC<TriviaScreenProps> = () => {
   return (
     <Layout>
       <Layout>
-        <Header>
-          <Navbar />
+        <Header style={styles.headerStyle}>
+          <Navbar
+            playerName="Edward andres"
+            earnings={data[0]}
+            dificulty="easy"
+          />
         </Header>
-        <Content>Content</Content>
+        <Content
+          style={{
+            backgroundImage: `url(${background})`,
+            backgroundRepeat: "no-repeat",
+            backgroundAttachment: "fixed",
+            backgroundPosition: "center",
+            opacity: "0.7",
+          }}
+        >
+          Content
+        </Content>
       </Layout>
       <Sider style={styles.sider} className="sider2">
         <List
           dataSource={data}
-          style={{...styles.list, justifyContent: 'center',flexDirection: 'column-reverse', overflow: 'auto'}}
+          style={{
+            ...styles.list,
+            justifyContent: "center",
+            flexDirection: "column-reverse",
+          }}
           renderItem={(item, idx) => (
             <List.Item
               style={{
-                backgroundColor: idx === data.length - 1 ? "#036364" : 'unset',
-                marginLeft: 25,
-                marginTop: 8,
-                borderRadius: 5,
-                borderBottomColor: '#030231',
-                display: "flex",
-                height: 30,
-                width: "80%",
-                justifyContent: "center",
-                alignItems: 'center',
+                ...styles.listItemStyle,
+                backgroundColor: idx === data.length - 1 ? "#02686c" : "unset",
               }}
             >
-              <p style={{ fontSize: 18,color: "#fff", margin: 0 }}>{`$ ${item}`}</p>
+              <p style={styles.listLabelStyle}>{`$ ${item}`}</p>
             </List.Item>
           )}
         />
