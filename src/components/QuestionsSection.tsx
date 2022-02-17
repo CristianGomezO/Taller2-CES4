fcec70ec60c502bd87bdfb5e62d24da865464083
CristianGomezO <cristiangomezosorio17@gmail.com>
2022-02-16 23:58:03 -0500

    juego finalizado

--- src/components/QuestionsSection.tsx
+++ src/components/QuestionsSection.tsx
@@ -2,6 +2,7 @@ import { Col, Row } from "antd";
 import React from "react";
 import { IQuestions } from "../types";
 import Timer from "./Timer";
+import he from "he";
 
 const styles = {
   mainContainer: {
@@ -37,40 +38,48 @@ const QuestionsSection: React.FC<QuestionsSectionProps> = ({
   onSelectAnswer,
   setTimeOut,
 }) => {
-  const returnCorrectAnswerClassName = React.useCallback((i: number): string => {
-    if (idxSelectedAnswer === i && isCorrectAnswer === undefined) {
-      return "selectedAnswer";
-    } else if (idxSelectedAnswer === i && isCorrectAnswer) {
-      return "greenBox"
-    } else if (idxSelectedAnswer === i &&  isCorrectAnswer === false) {
-      return "redBox"
-    }
-    return "answerBox";
-  }, [isCorrectAnswer, idxSelectedAnswer])
+  const returnCorrectAnswerClassName = React.useCallback(
+    (i: number): string => {
+      if (idxSelectedAnswer === i && isCorrectAnswer === undefined) {
+        return "selectedAnswer";
+      } else if (idxSelectedAnswer === i && isCorrectAnswer) {
+        return "greenBox";
+      } else if (idxSelectedAnswer === i && isCorrectAnswer === false) {
+        return "redBox";
+      }
+      return "answerBox";
+    },
+    [isCorrectAnswer, idxSelectedAnswer]
+  );
   return (
     <div style={styles.mainContainer}>
-      <Timer stopTimer={idxSelectedAnswer !== -1} setTimeOut={setTimeOut} questionNumber={level} />
+      <Timer
+        stopTimer={idxSelectedAnswer !== -1}
+        setTimeOut={setTimeOut}
+        questionNumber={level}
+      />
       <div style={styles.questionContainer}>
         <div className="question" style={{ textAlign: "center" }}>
-          <p className="answerText">{questions[level]?.question}</p>
+          <p className="answerText">{he.decode(questions[level]?.question)}</p>
         </div>
       </div>
       <div>
         <Row style={{ justifyContent: "center" }}>
           {answers.map((answer, i) => (
-            <Col
-              className={returnCorrectAnswerClassName(i)}
-              key={i}
-            >
+            <Col className={returnCorrectAnswerClassName(i)} key={i}>
               <div
                 onClick={() => {
                   if (idxSelectedAnswer === -1) {
                     onSelectAnswer(answer, i);
                   }
                 }}
-                className="answerTextContainer"
+                className={
+                  idxSelectedAnswer === -1
+                    ? "answerTextContainer answerTextContainerHover"
+                    : "answerTextContainer"
+                }
               >
-                <p className="answerText">{answer}</p>
+                <p className="answerText">{he.decode(answer)}</p>
               </div>
             </Col>
           ))}
